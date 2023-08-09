8223f8f8d800c70b9d6094273f2550fd10c6762d
Vembarasan Nagarajan <vembarasugn@gmil.com>
2023-08-10 01:23:26 +0530

    Services page check icon fix

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import Check from '/public/check.svg';
+import Check from '@/public/check.svg';
 
 const Services = () => {
   return (
