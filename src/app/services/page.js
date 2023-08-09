014ef6646753c522941787e6ed560a8a59c3b9c3
Vembarasan Nagarajan <vembarasugn@gmil.com>
2023-08-10 02:00:17 +0530

    Services page feature content updated

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -59,9 +59,9 @@ const Services = () => {
     <div className="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8">
       <div className="container mx-auto px-6 p-6 bg-white">
         <div className="mb-16 text-center">
-          <h4 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">Features</h4>
-          <p className="mt-2 text-5xl lg:text-7xl font-bold tracking-tight text-gray-900">
-            How we change the game
+          <h4 className="text-semibold text-gray-700 font-semibold tracking-wide uppercase">Features</h4>
+          <p className="mt-2 text-5xl lg:text-7xl font-bold tracking-tight text-teal-500">
+            How we can help your requirements
           </p>
         </div>
         <div className="flex flex-wrap my-12">
@@ -91,7 +91,7 @@ const Services = () => {
           </div>
           <div className="w-full border-b md:w-1/2 lg:w-1/3 lg:border-r lg:border-b-0 p-8">
             <div className="flex items-center mb-6">
-              <div className="ml-4 text-xl">Expertise</div>
+              <div className="ml-4 text-xl text-teal-500">Expertise</div>
             </div>
             <p className="leading-loose text-gray-500 text-justify">
             With over a decade of experience, our team comprises highly-skilled professionals with extensive knowledge in electrical engineering, solar power projects, and project management. Continuous learning and development are key to our success, and we stay abreast of the latest technologies, regulations, and industry trends. This expertise allows us to provide the best-in-class services to our clients, ensuring that their projects are executed to the highest standards.We strive to deliver the best results for every project and are continually improving our processes and technology to ensure that our customers get the most value for their money.
