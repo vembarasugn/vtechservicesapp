be2d7109fe58ef0a7385045aa8cf8b6452470f3e
Vembarasan Nagarajan <vembarasugn@gmil.com>
2023-08-10 21:52:17 +0530

    Landing page color updated

--- src/components/Landingpage/page.js
+++ src/components/Landingpage/page.js
@@ -10,7 +10,7 @@ const Landingpage = () => {
         <div className="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
           <h1 className="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">
             Vtech
-            <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-gray-500">
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-gray-500 to-blue-400">
             Renewable
             </span><br/>
              services!  
